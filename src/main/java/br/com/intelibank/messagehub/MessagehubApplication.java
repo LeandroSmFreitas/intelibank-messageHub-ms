2be198eddcabfbc4bf4a3b7c6e74b3f9c809ad9f
Leandro Simões <Leandrosimoes42@gmail.com>
2024-11-29 18:20:18 -0300

    feat: initial configs

---
+++ src/main/java/br/com/intelibank/messagehub/MessagehubApplication.java
@@ -0,0 +1,13 @@
+package br.com.intelibank.messagehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/intelibank/messagehub/MessagehubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intelibank.messagehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
